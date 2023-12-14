eec3789f8a8b39e606bd7b9ac2326b64c8e40bf4
fazleh-rakib <rakibfazleh@gmail.com>
2023-12-14 12:03:44 +0600

    creating simple shopping web

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='app'>
+        <Header />
+      </div>
+     
+    </>
+  )
+}
+
+export default App
