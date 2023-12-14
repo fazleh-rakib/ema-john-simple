560a9787c2e572838790680c1d7e7a467e98bd29
fazleh-rakib <rakibfazleh@gmail.com>
2023-12-14 13:56:48 +0600

    added product

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,11 +3,17 @@ import './Product.css'
 
 const Product = (props) => {
     console.log(props.product);
-    const {name,price,img,seller,quantity} = props.product
+    const {name,price,img,seller,quantity,ratings} = props.product
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <h2>Name : {name}  </h2>
+            <div className='product-info'>
+            <h6 className='product-name'>{name}</h6>
+            <p>Price :${price}</p>
+            <p>Manufacturer : {seller}</p>
+            <p>Ratings : {ratings}</p>
+            </div>
+            <button className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
